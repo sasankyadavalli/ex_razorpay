4d51aacdf92833f74a5340d762b06910d24bcd07
Sasank Yadavalli <yadavallisasank@gmail.com>
2017-11-01 19:49:24 -0700

    Add description/0 and package/0

--- mix.exs
+++ mix.exs
@@ -6,6 +6,9 @@ defmodule ExRazorpay.Mixfile do
       app: :ex_razorpay,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: "Razorpay API library for Elixir",
+      source_url: "https://github.com/sasankyadavalli/ex_razorpay",
+      package: package(),
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -25,6 +28,21 @@ defmodule ExRazorpay.Mixfile do
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Razorpay API client for Elixir"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "ex_razorpay",
+      # These are the default files included in the package
+      maintainers: ["Sasank Yadavalli"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sasankyadavalli/ex_razorpay"}
     ]
   end
 end
