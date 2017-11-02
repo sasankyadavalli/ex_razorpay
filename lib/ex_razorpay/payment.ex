a51fee0093055855ec589648f4dd30ba0fd551cd
Sasank Yadavalli <yadavallisasank@gmail.com>
2017-11-02 10:49:17 -0700

    Add documentation to list_payments/1

--- lib/ex_razorpay/payment.ex
+++ lib/ex_razorpay/payment.ex
@@ -1,9 +1,40 @@
 defmodule ExRazorpay.Payments do
-  @moduledoc false
+  @moduledoc """
+    Handles Razorpay payment requests like list_payments, fetch_payment, create payment etc.
+  """
 
   @key :ex_razorpay |> Application.fetch_env!(:key)
   @secret :ex_razorpay |> Application.fetch_env!(:secret)
 
+  @doc """
+  Retrives list of payments based on optional parameters. 
+  By default this returns recent 10 payments. 
+    
+  Optional parameters it supports are:
+  * `from`: The timestamp in seconds after which the payments were created. Accepts only `timestamp (epoch)`
+  * `to`:   The timestamp in seconds before which payments were created. Accepts only `timestamp (epoch)`
+  * `count`: The number of payments to fetch. Accepts only `integer`
+  * `skip`:  The number of payments to be skipped. Accepts only `integer`
+
+  Returns `{:ok, results}` on success, else `{:error, reason}`
+
+  ## Examples
+
+      iex> ExRazorpay.Payments.list_payments([from: 1483438659, to: 1509493250, count: 1])
+      {:ok,
+        %{"count" => 1, "entity" => "collection",
+          "items" => [%{"amount" => 50000, "amount_refunded" => 0, "bank" => nil,
+          "captured" => true, "card_id" => "card_8vzYOiv7xQgMDQ",
+          "contact" => "+917995738307", "created_at" => 1509493250,
+          "currency" => "INR", "description" => "Purchase Description",
+          "email" => "support@razorpay.com", "entity" => "payment",
+          "error_code" => nil, "error_description" => nil, "fee" => 1250,
+          "id" => "pay_8vzYOi5UjY2rmX", "international" => false,
+          "invoice_id" => nil, "method" => "card", "notes" => [], "order_id" => nil,
+          "refund_status" => nil, "status" => "captured", "tax" => 0, "vpa" => nil,
+          "wallet" => nil}]}}
+
+  """
   def list_payments(options \\ []) when is_list(options) do
     "https://api.razorpay.com/v1/payments"
     |> format_url(options)
@@ -11,7 +42,7 @@ defmodule ExRazorpay.Payments do
     |> parse_response()
   end
 
-  def get_payment(id) when is_binary(id) do
+  def fetch_payment(id) when is_binary(id) do
     "https://api.razorpay.com/v1/payments/#{id}"
     |> HTTPoison.get([], hackney: [basic_auth: {@key, @secret}])
     |> parse_response()
